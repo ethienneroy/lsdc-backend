caaca68c2a05d2e6bc6145143789f74719edc4aa
Ethienne Roy <ethienner@gmail.com>
2021-06-01 09:50:00 -0400

    Added database config for production

---
+++ config/env/production/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('MY_HEROKU_URL'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '08377872ea91e84e78399f10dc1ae3a9'),
+    },
+  },
+});
