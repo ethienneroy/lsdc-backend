3d4bd1899d158f00e6a96d6b52a803b3aef502ca
Ethienne Roy <ethienner@gmail.com>
2021-06-02 20:12:56 -0400

    Added categories content type + changed prod config to use env vars

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,18 +1,18 @@
-const parse = require('pg-connection-string').parse;
+const parse = require("pg-connection-string").parse;
 const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'postgres',
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
+        client: "postgres",
+        host: process.env.host,
+        port: process.env.port,
+        database: process.env.database,
+        username: process.env.db_user,
+        password: process.env.db_password,
         ssl: {
           rejectUnauthorized: false,
         },
