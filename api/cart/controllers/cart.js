7f7e6f54300e09d387c9e40d70fc13294dccc78b
Ethienne Roy <ethienner@gmail.com>
2021-07-09 08:42:22 -0400

    added stripe integration for payment - new api key

--- api/cart/controllers/cart.js
+++ api/cart/controllers/cart.js
@@ -4,8 +4,10 @@
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
-const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
-const YOUR_DOMAIN = 'http://localhost:3000'
+const stripe = require('stripe')('sk_test_51J0yVyKxbju7nISONWbOx0ntsoHd6Nf7YjZ6wkxqB91ty5i4RlVWeLHyUXZ0JpSDx6B1nwQ25rFXEoxd3CMjbTOU00LneYuN4Z');
+
+// const YOUR_DOMAIN = 'http://localhost:3000'
+const YOUR_DOMAIN = 'https://les-savons-de-celine-web.herokuapp.com/'
 module.exports = {
   checkout: async (ctx) => {
     console.log(ctx.request.body)
@@ -49,6 +51,8 @@ module.exports = {
       cancel_url: `${YOUR_DOMAIN}?canceled=true`,
     });
     ctx.statusCode = 303
+
+    console.log(session)
     ctx.send(session.url)
   }
 };
